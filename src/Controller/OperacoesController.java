46f4e8e5f6d94e8c88784f932bd26176003ad6f9
Edu <honorio.edu2020@gmail.com>
2026-03-02 17:33:01 -0300

    Fim

--- src/Controller/OperacoesController.java
+++ src/Controller/OperacoesController.java
@@ -3,9 +3,11 @@ package Controller;
 public class OperacoesController {
 	public int Fib(int x) {
 		//Condicao de parada
+		//Se o valor for 0 ou 1, retornar o mesmo valor
 		if (x==0 || x ==1) {
 			return x;
 		}else {
+			// Fib(6) -> fib(5) + fib(4) ->fib(4) +fib(3) ...
 			return Fib(x-1) + Fib(x-2);
 		}
 	}
